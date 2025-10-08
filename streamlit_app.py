7d96566704d7cec2efffab4661a838e5951f0141
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2025-10-08 21:36:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+# Page config
+st.set_page_config(page_title="IPL Broadcast Dashboard", layout="wide")
 
-st.write('Hello world!')
+# Load dataset
+@st.cache_data
+def load_data():
+    path = "Dataset/ipl_bbb_2021_25.csv"
+    df = pd.read_csv(path)
+    return df
+
+df = load_data()
+
+st.title("📊 IPL 2021–2025 Broadcast Dashboard")
+st.write("Data loaded successfully. Preview below 👇")
+
+st.dataframe(df.head())
